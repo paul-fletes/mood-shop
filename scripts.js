0f2722023c7b5409edce500be196216f13e2b593
Paul Fletes <paul.fletes@gmail.com>
2022-09-27 13:41:59 -0500

    display moods with button using js

--- scripts.js
+++ scripts.js
@@ -12,4 +12,15 @@ for (let i = 0; i < data.length; i += 1) {
     newDiv.appendChild(img)
     console.log(img)
     itemsContainer.appendChild(newDiv)
+    const desc = document.createElement('p');
+    desc.innerText = data[i].desc
+    newDiv.appendChild(desc)
+    const price = document.createElement('p');
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+    const button = document.createElement('button');
+    button.id = data[i].name
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
 }
