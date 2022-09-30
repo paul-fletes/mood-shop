ac062198c1d247192067166568418210d25242dc
Paul Fletes <paul.fletes@gmail.com>
2022-09-30 09:38:22 -0500

    display cart items in console

--- scripts.js
+++ scripts.js
@@ -28,18 +28,48 @@ for (let i = 0; i < data.length; i += 1) {
 const cart = [];
 
 
-
 function addItem(name, price) {
-    const item = { name: name, price: price, qty: 1 }
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+            cart[i].qty++;
+            return
+        }
+    }
+    const item = { name, price, qty: 1 }
     cart.push(item);
 }
 
 function showItems() {
-    console.log(`You have ${cart.length} items in your cart.`);
+    console.log(`You have ${getQty()} items in your cart.`);
+
+    for (let i = 0; i < cart.length; i++) {
+        console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+    }
+
+    console.log(`Cart total is: $${getTotal()}.`)
+}
+
+function getQty() {
+    let qty = 0;
+    for (let i = 0; i < cart.length; i++) {
+        qty += cart[i].qty
+    }
+    return qty
+}
+
+function getTotal() {
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        total += cart[i].price * cart[i].qty
+    }
+    return total.toFixed(2)
 }
 
 addItem('Apple', 0.99);
 addItem('Orange', 1.29);
 addItem('Opinion', 0.02);
+addItem('Apple', 0.99);
 addItem('Frisbee', 9.99);
+addItem('Apple', 0.99);
+addItem('Orange', 1.29);
 showItems();
